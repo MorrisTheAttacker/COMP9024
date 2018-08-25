ded9a9d4a678bd8714c7bbe16c8a1ba5d9d3a837
MorrisTheAttacker <42224415+MorrisTheAttacker@users.noreply.github.com>
2018-08-25 15:06:45 +1000

    Update main.c
    Task 1 completed. 8/25 afternoon

--- starterCode/main.c
+++ starterCode/main.c
@@ -10,13 +10,15 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <ctype.h>
-#include <math.h>
+//#include <math.h>
 
 #include "cardRecord.h"
 #include "cardLL.h"
 
 void printHelp();
 void CardLinkedListProcessing();
+int get_int_len();
+void clean_stdin();
 
 int main(int argc, char *argv[]) {
    if (argc == 2) {
@@ -42,31 +44,35 @@ int main(int argc, char *argv[]) {
          //in this case, "01234567" starts with 0 and length is 7
          //it can not satify the condition. On the other hand, "012345678"
          //seems starts with 0 and length is 9. In fact it equals to "12345678"
-         while(scanf("%d",&card_id) == 0 || 1 + (int)log10(card_id) != 8){
+         while(scanf("%d",&card_id) == 0 || get_int_len(card_id) != 8){
             printf("Not valid. Enter a valid value: ");
-            fflush(stdin); //empty the input
+            //fflush(stdin); //empty the input
+	    clean_stdin();
          }
          ID_array[i] = card_id;
          printf("Enter amount: ");
          //check if the input is a float number, and if the floor and roof is -2.35 and 250
          while(scanf("%f",&amount) == 0 || amount < -2.35 || amount > 250){
             printf("Not valid. Enter a valid value: ");
-            fflush(stdin); //empty the empty
+            //fflush(stdin); //empty the empty
+	    clean_stdin();
          }
          amount_array[i] = amount;
       }
       for (int k = 0; k < ID_array_len; k++){
          printf("-----------------\n");
          printf("Card ID: %d\n",ID_array[k]);
-         printf("Balance: $%.2f\n",amount_array[k]);
+	 if (amount_array[k] < 0){
+	     printf("Balance: -$%.2f\n",-amount_array[k]);
+	 }else{printf("Balance: $%.2f\n",amount_array[k]);}
          if (amount_array[k] < 5){
-            printf("Low Balance\n");
+            printf("Low balance\n");
          }
          printf("-----------------\n");
          total += amount_array[k];
       }
-      avgbalance = total/argc;
-      printf("Number of cards on file: %d\n",argc);
+      avgbalance = total/ID_array_len;
+      printf("Number of cards on file: %d\n",ID_array_len);
       printf("Average balance: $%.2f\n",avgbalance);
       free(ID_array); //free the memory
       free(amount_array); //free the memory
@@ -74,6 +80,20 @@ int main(int argc, char *argv[]) {
       CardLinkedListProcessing();
    }
    return 0;
+}
+
+void clean_stdin(void)
+{
+    int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+int get_int_len (int value){
+  int l=1;
+  while(value>9){ l++; value/=10; }
+  return l;
 }
 
 /* Code for Stages 2 and 3 starts here */
